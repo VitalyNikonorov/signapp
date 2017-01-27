358b648e8adc91985f9f02d4224862d67fb0f9a7
VitalyNikonorov <nik.vitaly@mail.ru>
2017-01-27 12:12:13 +0300

    add auth screen activity layout and view

--- app/src/main/java/nikonorov/net/signapp/authscreen/view/AuthActivity.java
+++ app/src/main/java/nikonorov/net/signapp/authscreen/view/AuthActivity.java
@@ -1,6 +1,10 @@
 package nikonorov.net.signapp.authscreen.view;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import nikonorov.net.signapp.R;
 
 /**
  * Created by vitaly on 27.01.17.
@@ -8,6 +12,12 @@ import android.support.v7.app.AppCompatActivity;
 
 public class AuthActivity extends AppCompatActivity implements ViewAuthScreen {
 
+    private View authScreenContainer;
 
-
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth_screen);
+        authScreenContainer = findViewById(R.id.auth_screen_container);
+    }
 }
