2d8c4d369c225a69e22b559b05360f74279f02e7
VitalyNikonorov <nik.vitaly@mail.ru>
2017-01-27 15:46:56 +0300

    add permament login screen

---
+++ app/src/main/java/nikonorov/net/signapp/authscreen/view/fragments/FragmentPermamentLogin.java
@@ -0,0 +1,24 @@
+package nikonorov.net.signapp.authscreen.view.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import nikonorov.net.signapp.R;
+
+/**
+ * Created by vitaly on 27.01.17.
+ */
+
+public class FragmentPermamentLogin extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        return view;
+    }
+}
