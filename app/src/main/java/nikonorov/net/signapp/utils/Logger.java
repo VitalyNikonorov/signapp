b3980a27145f556c13846fb11a718eae402d458e
VitalyNikonorov <nik.vitaly@mail.ru>
2017-01-28 00:13:58 +0300

    add logger class

---
+++ app/src/main/java/nikonorov/net/signapp/utils/Logger.java
@@ -0,0 +1,19 @@
+package nikonorov.net.signapp.utils;
+
+import android.util.Log;
+
+/**
+ * Created by vitaly on 28.01.17.
+ */
+
+public class Logger {
+
+    public static boolean isLoggingNeed = true;
+
+    public static void d(String classFrom, String msg){
+        if (isLoggingNeed){
+            Log.d(classFrom, msg);
+        }
+    }
+
+}
