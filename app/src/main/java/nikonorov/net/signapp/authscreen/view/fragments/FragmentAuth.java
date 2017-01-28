404c690300c8ec4e675b72663edd30534679e26d
VitalyNikonorov <nik.vitaly@mail.ru>
2017-01-28 15:52:24 +0300

    add enabling and disabling main action button

--- app/src/main/java/nikonorov/net/signapp/authscreen/view/fragments/FragmentAuth.java
+++ app/src/main/java/nikonorov/net/signapp/authscreen/view/fragments/FragmentAuth.java
@@ -4,6 +4,9 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +17,12 @@ import android.widget.TextView;
 import nikonorov.net.signapp.R;
 import nikonorov.net.signapp.authscreen.model.AuthData;
 import nikonorov.net.signapp.authscreen.view.AuthActivity;
-import nikonorov.net.signapp.utils.Logger;
 
 /**
  * Created by vitaly on 27.01.17.
  */
 
-public class FragmentAuth extends Fragment {
+public class FragmentAuth extends Fragment implements TextWatcher {
 
     private TextView description;
     private EditText loginField;
@@ -62,10 +64,31 @@ public class FragmentAuth extends Fragment {
 
         description = (TextView) view.findViewById(R.id.login_description);
         loginField = (EditText) view.findViewById(R.id.login_login_field);
+
+        loginField.addTextChangedListener(this);
+
         passField = (EditText) view.findViewById(R.id.login_pass_field);
+        passField.addTextChangedListener(this);
+
         loginTip = (TextView) view.findViewById(R.id.login_tip_tv);
 
         return view;
+    }
+
+    private void checkMainBtnState(){
+        if (TextUtils.isEmpty(loginField.getText())){
+            mainActionBtn.setEnabled(false);
+        } else {
+            if (passField.getVisibility() == View.VISIBLE){
+                if (TextUtils.isEmpty(passField.getText())){
+                    mainActionBtn.setEnabled(false);
+                } else {
+                    mainActionBtn.setEnabled(true);
+                }
+            } else {
+                mainActionBtn.setEnabled(true);
+            }
+        }
     }
 
     public AuthData getAuthData(){
@@ -96,5 +119,21 @@ public class FragmentAuth extends Fragment {
         mainActionBtn.setText(type.mainBtnCaption);
         additionalBtn.setText(type.additionalBtnCaption);
         loginTip.setText(type.tip);
+        checkMainBtnState();
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        checkMainBtnState();
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        checkMainBtnState();
     }
 }
